969674a672ac0c4e5f728071350e9887240d006e
Your Name <you@example.com>
2024-11-23 02:45:54 -0400

    Display Additional Countries Information in a Table

--- world.php
+++ world.php
@@ -23,12 +23,28 @@ try {
     die();
 }
 ?>
-<ul>
+
 <?php if ($results): ?>
-    <?php foreach ($results as $row): ?>
-        <li><?= htmlspecialchars($row['name']) . ' is ruled by ' . htmlspecialchars($row['head_of_state']); ?></li>
-    <?php endforeach; ?>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Country Name</th>
+                <th>Continent</th>
+                <th>Independence Year</th>
+                <th>Head of State</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($results as $row): ?>
+                <tr>
+                    <td><?= htmlspecialchars($row['name']); ?></td>
+                    <td><?= htmlspecialchars($row['continent']); ?></td>
+                    <td><?= htmlspecialchars($row['independence_year'] ?: 'N/A'); ?></td>
+                    <td><?= htmlspecialchars($row['head_of_state'] ?: 'N/A'); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 <?php else: ?>
-    <li>No results found.</li>
+    <p>No results found.</p>
 <?php endif; ?>
-</ul>
